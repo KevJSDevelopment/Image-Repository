b7061c493d65325e003647940c51064e6a4a7547
KevJSDevelopment <Kshhn12@gmail.com>
2021-01-11 11:53:32 -0500

    added image s to photo table

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,15 +1,15 @@
 class PhotosController < ApplicationController
 
     def search
-
+        
     end
 
     def create
-
+        photo = Photo.new(image: params[:image])
     end
 
     def show
-
+        photo = Photo.find(params[:id])
     end
 
 end
